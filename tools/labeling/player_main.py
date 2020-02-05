bb2fa9f22f274e908321332250f2a5efc59d88d6
marc-antoine <maheux.marcantoine@gmail.com>
2020-02-05 16:53:28 -0500

    Ajout d'un joueur de vidéo.

---
+++ tools/labeling/player_main.py
@@ -0,0 +1,14 @@
+import sys
+
+from PySide2.QtWidgets import QApplication
+
+from player_interface import PlayerInterface
+
+def main():
+    app = QApplication(sys.argv)
+    interface = PlayerInterface()
+    interface.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
