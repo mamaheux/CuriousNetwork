6b306334ff34f365cce2f433a32c386992f19d36
marc-antoine <maheux.marcantoine@gmail.com>
2020-02-05 10:46:58 -0500

    Ajout de la base de l'interface

---
+++ tools/labeling/main.py
@@ -0,0 +1,14 @@
+import sys
+
+from PySide2.QtWidgets import QApplication
+
+from labeling_interface import LabelingInterface
+
+def main():
+    app = QApplication(sys.argv)
+    interface = LabelingInterface()
+    interface.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
