f3d6b19dfef8ad7e7d6b00464a6a30b8696ff84b
Gabriel Gibeau Sanchez <ggibeau@doubleg.info>
2020-02-05 10:44:15 -0500

    Complete IO model & tester - first draw

--- tools/labeling/IO_model.py
+++ tools/labeling/IO_model.py
@@ -1,11 +1,12 @@
 import os
 import numpy as np
 
+
 class FileHandler:
 
     def __init__(self, path):
         if path == '':
-            raise IOError('A path must be given to instanciate a FileHandler object')
+            raise IOError('A valid path must be given to instanciate a FileHandler object')
         self.directory = path
         self.frames = os.listdir(path)
         self.frame_counter = 0
@@ -22,8 +23,11 @@ class FileHandler:
             self.frame_counter -= 1
         return p_frame
 
-    def tags_to_file(self, annotated_matrix):
-        if isinstance(annotated_matrix, list):
-            np.savetxt('array.csv', [annotated_matrix], delimiter=',', fmt='%d')
-        else:
-            raise TypeError('annotated_matrix must be a list')
+    def set_frames_directory(self, path):
+        self.directory = path
+        self.frames = os.listdir(self.directory)
+        self.frame_counter = 0
+
+    def tags_to_file(self, annotated_matrix, path):
+        print('Annotated matrix is: {}'.format(annotated_matrix))
+        np.savetxt(path, annotated_matrix, delimiter=',', fmt='%f')
--- tools/labeling/IO_model_tester.py
+++ tools/labeling/IO_model_tester.py
@@ -1,4 +1,6 @@
 from IO_model import FileHandler
+import numpy as np
+import os
 
 model = FileHandler('/home/ggs22/Pictures')
 
@@ -8,3 +10,17 @@ print(model.next_frame())
 print(model.next_frame())
 print(model.next_frame())
 
+print(model.previous_frame())
+print(model.previous_frame())
+print(model.previous_frame())
+print(model.previous_frame())
+print(model.previous_frame())
+
+tags = np.zeros((9,16), dtype=float)
+# tags = np.zeros(9, dtype=int)
+
+print(tags.shape)
+print(type(tags))
+print(type(tags) == 'class \'numpy.ndarray\'')
+
+model.tags_to_file(tags, './test.csv')
