facaaa99ae381ada695a1e54147fb8f59d834e7d
Gabriel Gibeau Sanchez <ggs8922@gmail.com>
2020-04-05 00:40:46 -0400

    annotations on video working

--- tools/dataset_jpg_to_mp4/database_jpg_to_mp4.py
+++ tools/dataset_jpg_to_mp4/database_jpg_to_mp4.py
@@ -1,8 +1,6 @@
 import cv2
-import numpy as np
-import os
-from os.path import isfile, join
 from IO_model import FileHandler
+import numpy as np
 
 pathIn= '/home/ggs22/repos/CuriousNetwork/run_output'
 
@@ -13,9 +11,25 @@ fps = 24
 
 size = (1920, 1080)
 out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'3IVD'), fps, size)
+alpha = 0.85
+
+
+
 for image_path, image_annotation in file_handle:
+
     # writing to a image array
-    out.write(cv2.imread(image_path))
+    img = cv2.imread(image_path)
+
+    annotations = np.where(image_annotation == 1)
+    for index in range(annotations[0].shape[0]):
+        x = annotations[1][index] * 120
+        y = annotations[0][index] * 120
+        layer = np.zeros(img.shape, dtype=np.uint8)
+        cv2.fillConvexPoly(layer, np.array([[x, y], [x + 120, y], [x + 120, y + 120], [x, y + 120]]),
+                           (0, 0, 255), 8, 0)
+        img = cv2.addWeighted(layer, alpha, img, 1, 0.0)
+
+    out.write(img)
 
 
 
