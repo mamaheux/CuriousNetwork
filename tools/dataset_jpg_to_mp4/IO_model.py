be2a0ea2eb780e528d6bcd25154a025db7b48e4d
Gabriel Gibeau Sanchez <ggs8922@gmail.com>
2020-04-05 21:30:33 -0400

    path are passed as script arguments

--- tools/dataset_jpg_to_mp4/IO_model.py
+++ tools/dataset_jpg_to_mp4/IO_model.py
@@ -1,24 +1,49 @@
 import os
 import glob
 import numpy as np
+import cv2
 
 MAX_FRAME_COUNT = 1000000000
 
 class FileHandler:
 
-    def __init__(self, path=os.curdir):
-        self._directory = path
-        self._frames = glob.glob(os.path.join(path, '*.jpg'))
-        self._frames.sort(key=self._filename_sort_key)
+    def __init__(self, path_curiosity, path_test):
+        self._directory_curiosity = path_curiosity
+        self._directory_test = path_test
+
+        self._frames_curiosity = glob.glob(os.path.join(path_curiosity, '*.jpg'))
+        self._frames_curiosity.sort(key=self._filename_sort_key)
+
+        self._frames_test = glob.glob(os.path.join(path_test, '*.jpg'))
+        self._frames_test.sort(key=self._filename_sort_key)
 
     def __len__(self):
-        return len(self._frames)
+        return len(self._frames_curiosity)
 
     def __getitem__(self, i):
-        return os.path.join(self._directory, self._frames[i]), self._load_annonated_frame(i)
+        return os.path.join(self._directory_curiosity, self._frames_curiosity[i]),\
+               self._load_annonated_frame(i), \
+               os.path.join(self._directory_test, self._frames_test[i]), \
+               self._load_test_frame(i)
+
+    def get_annotated_pair(self, path):
+        annotated_frames = glob.glob(os.path.join(path, 'annoted_img_*.jpg'))
+        annotated_frames.sort()
+        test_frames = glob.glob(os.path.join(path, 'test_img_*.jpg'))
+        test_frames.sort()
+        assert len(annotated_frames) == len(test_frames)
+
+        return test_frames, annotated_frames
 
     def _load_annonated_frame(self, i):
-        path = os.path.join(self._directory, str.replace(self._frames[i], '.jpg', '.txt'))
+        path = os.path.join(self._directory_curiosity, str.replace(self._frames_curiosity[i], '.jpg', '.txt'))
+        if os.path.exists(path):
+            return np.loadtxt(path, delimiter=',').astype(int)
+        else:
+            return np.zeros(shape=(16, 9), dtype=int)
+
+    def _load_test_frame(self, i):
+        path = os.path.join(self._directory_test, str.replace(self._frames_test[i], '.jpg', '.txt'))
         if os.path.exists(path):
             return np.loadtxt(path, delimiter=',').astype(int)
         else:
@@ -30,3 +55,39 @@ class FileHandler:
         x = x.split('-')
 
         return MAX_FRAME_COUNT * int(x[0]) + int(x[1])
+
+    def generate_annotated_frames_pairs(self, output_path, alpha=0.7):
+        delta = 120
+
+        if not os.path.isdir(output_path):
+            os.mkdir(output_path)
+
+        for index_global, (curiosity_img_path, curiosity_annotation, test_img_path, test_annotation) in enumerate(self):
+
+            filename_curiosity = os.path.join(output_path, f'annoted_img_{index_global:05d}.jpg')
+            filename_test = os.path.join(output_path, f'test_img_{index_global:05d}.jpg')
+
+            img_curiosity = cv2.imread(curiosity_img_path)
+            img_test = cv2.imread(test_img_path)
+
+            annotations = np.where(test_annotation == 1)
+            for index in range(annotations[0].shape[0]):
+                x = annotations[1][index] * delta
+                y = annotations[0][index] * delta
+                layer = np.zeros(img_test.shape, dtype=np.uint8)
+                cv2.fillConvexPoly(layer, np.array([[x, y], [x + delta, y], [x + delta, y + delta], [x, y + delta]]),
+                                   (0, 0, 255), 8, 0)
+                img_test = cv2.addWeighted(layer, alpha, img_test, 1, 0.0)
+
+            annotations = np.where(curiosity_annotation == 1)
+            for index in range(annotations[0].shape[0]):
+                x = annotations[1][index] * delta
+                y = annotations[0][index] * delta
+                layer = np.zeros(img_curiosity.shape, dtype=np.uint8)
+                cv2.fillConvexPoly(layer, np.array([[x, y], [x + delta, y], [x + delta, y + delta], [x, y + delta]]),
+                                   (0, 0, 255), 8, 0)
+                img_curiosity = cv2.addWeighted(layer, alpha, img_curiosity, 1, 0.0)
+
+            print(f'dual (Curiosity & test) annotation overlay: {index_global+1}/{len(self)}')
+            assert cv2.imwrite(filename_curiosity, img_curiosity)
+            assert cv2.imwrite(filename_test, img_test)
--- tools/dataset_jpg_to_mp4/database_jpg_to_mp4.py
+++ tools/dataset_jpg_to_mp4/database_jpg_to_mp4.py
@@ -1,36 +1,62 @@
+import argparse
 import cv2
+from PIL import Image, ImageDraw, ImageFont
 from IO_model import FileHandler
+import os
 import numpy as np
 
-pathIn= '/home/ggs22/repos/CuriousNetwork/run_output'
 
-file_handle = FileHandler(pathIn)
+def main():
+    parser = argparse.ArgumentParser(description='Convert jpg to avi')
+    parser.add_argument('-i', '--curiosity_path', type=str, help='Choose the curiosity database path', required=True)
+    parser.add_argument('-t', '--test_path', type=str, help='Choose test database path', required=True)
+    parser.add_argument('-o', '--output_path', type=str, help='Choose the output path', required=True)
 
-pathOut = 'video.avi'
-fps = 24
-
-size = (1920, 1080)
-out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'3IVD'), fps, size)
-alpha = 0.85
-
+    args = parser.parse_args()
+    convert_to_video(args)
 
 
-for image_path, image_annotation in file_handle:
-
-    # writing to a image array
-    img = cv2.imread(image_path)
-
-    annotations = np.where(image_annotation == 1)
-    for index in range(annotations[0].shape[0]):
-        x = annotations[1][index] * 120
-        y = annotations[0][index] * 120
-        layer = np.zeros(img.shape, dtype=np.uint8)
-        cv2.fillConvexPoly(layer, np.array([[x, y], [x + 120, y], [x + 120, y + 120], [x, y + 120]]),
-                           (0, 0, 255), 8, 0)
-        img = cv2.addWeighted(layer, alpha, img, 1, 0.0)
-
-    out.write(img)
+def get_identifying_frame(size):
+    img = np.zeros(size, dtype=np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("Carlito-Regular.ttf", 45, encoding='utf-8')
+    draw.text((275, 270), 'Vidéo annotée manuellement: ', font=font)
+    draw.text((275, 810), 'Vidéo annotée par le \nmodèle (backend VGG-16): ', font=font)
+    return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
 
+def convert_to_video(args):
+    pathOut = os.path.join(args.output_path, 'video.avi')
+    file_handle = FileHandler(args.curiosity_path, args.test_path)
 
-out.release()
+    fps = 24
+    size = (1920, 1080)
+    out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'3IVD'), fps, size)
+    alpha = 0.85
+
+    file_handle.generate_annotated_frames_pairs(args.output_path, alpha)
+    paires = file_handle.get_annotated_pair(args.output_path)
+
+    for index, _ in enumerate(paires[0]):
+        test_img = cv2.imread(paires[0][index])
+        curiosity_img = cv2.imread(paires[1][index])
+        assert test_img.shape == curiosity_img.shape
+
+        text_layer = get_identifying_frame((1080, 960, 3))
+
+        test_img = cv2.resize(test_img, (np.int_(size[0]/2), np.int_(size[1]/2)))
+        curiosity_img = cv2.resize(curiosity_img, (np.int_(size[0] /2), np.int_(size[1]/2)))
+
+        im_v = cv2.vconcat([test_img, curiosity_img])
+        im_v = cv2.hconcat([text_layer, im_v])
+
+        out.write(im_v)
+        print(f'Creating video frame: {index + 1}/{len(paires[0])}')
+
+    out.release()
+
+
+if __name__ == '__main__':
+    main()
