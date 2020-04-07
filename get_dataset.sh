294919087078a5e88c04a12588cacabd3fc4baf5
Gabriel Gibeau Sanchez <ggs8922@gmail.com>
2020-04-07 17:35:50 -0400

    corrected extracting command

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir dataset
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1wJXbKPE8-LNMeCi6SOoZ0sPyqbJFfidY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1wJXbKPE8-LNMeCi6SOoZ0sPyqbJFfidY" -O dataset.tar && rm -rf /tmp/cookies.txt
 tar -xf dataset.tar
 rm dataset.tar
